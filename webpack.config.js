23f6dfdb817b7c53554ea4f5ccfb78bb6b331a71
Structure88 <ibsj88@gmail.com>
2021-03-19 23:02:44 +0800

    add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   // mode: 'production',
   entry: './src/main.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'js/bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      inject: 'body',
+      scriptLoading: 'blocking'
+    })
+  ]
 };
